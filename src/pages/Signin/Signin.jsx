27a29a5e5476b3a152584f1f3f96b61534779ba7
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-19 04:56:29 +0200

    Added some more styling for sign in page

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,5 +1,6 @@
-import { Button, TextField } from "@mui/material";
+import { Button, capitalize, TextField } from "@mui/material";
 import React from "react";
+import "./Signin.css";
 
 const Signin = () => {
   return (
@@ -7,29 +8,37 @@ const Signin = () => {
       <div className="signin-center-content flex flex-col items-center justify-center w-3/5 m-auto">
         <div className="flex">
           <div className="signin-left-side">
-            <h3>Welcome back!</h3>
+            <h3 className="">Welcome back!</h3>
             <p>Let today be as a step to empower growth!</p>
 
-            <div className="signin-form">
+            <div className="signin-form flex flex-col gap-2">
               <label htmlFor="username" className="signin-label">
                 Username
               </label>
               <TextField id="username" variant="outlined" />
 
               <label htmlFor="password" className="signin-label">
-                Username
+                Password
               </label>
-              <TextField id="username" variant="outlined" />
+              <TextField id="username" type="password" variant="outlined" />
             </div>
             <p>
               Forgot your password?{" "}
               <button className="signin-reset-button">Reset</button>
             </p>
-            <Button variant="contained">Sign in</Button>
+            <Button
+              variant="contained"
+              style={{
+                width: "100%",
+                fontWeight: "bold",
+                fontSize: 24,
+                textTransform: "none",
+              }}
+            >
+              Sign in
+            </Button>
           </div>
-          <div className="signin-right-side">
-            <strong>Hi</strong>
-          </div>
+          <div className="signin-right-side"></div>
         </div>
         <p>
           No account ? No problem,{" "}
