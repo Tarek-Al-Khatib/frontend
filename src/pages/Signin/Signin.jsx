edaeffb80c7589b1becd511a5d01af5784da7e43
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-19 04:40:38 +0200

    Used tailwindcss for some styling in signin page

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -3,35 +3,39 @@ import React from "react";
 
 const Signin = () => {
   return (
-    <div className="signin-container">
-      <div className="signin-center-content">
-        <div className="signin-left-side">
-          <h3>Welcome back!</h3>
-          <p>Let today be as a step to empower growth!</p>
+    <div className="signin-container bg-">
+      <div className="signin-center-content flex flex-col items-center justify-center w-3/5 m-auto">
+        <div className="flex">
+          <div className="signin-left-side">
+            <h3>Welcome back!</h3>
+            <p>Let today be as a step to empower growth!</p>
 
-          <div className="signin-form">
-            <label htmlFor="username" className="signin-label">
-              Username
-            </label>
-            <TextField id="username" variant="outlined" />
+            <div className="signin-form">
+              <label htmlFor="username" className="signin-label">
+                Username
+              </label>
+              <TextField id="username" variant="outlined" />
 
-            <label htmlFor="password" className="signin-label">
-              Username
-            </label>
-            <TextField id="username" variant="outlined" />
+              <label htmlFor="password" className="signin-label">
+                Username
+              </label>
+              <TextField id="username" variant="outlined" />
+            </div>
+            <p>
+              Forgot your password?{" "}
+              <button className="signin-reset-button">Reset</button>
+            </p>
+            <Button variant="contained">Sign in</Button>
           </div>
-          <p>
-            Forgot your password?{" "}
-            <button className="signin-reset-button">Reset</button>
-          </p>
-          <Button variant="contained">Sign in</Button>
+          <div className="signin-right-side">
+            <strong>Hi</strong>
+          </div>
         </div>
-        <div className="signin-right-side"></div>
+        <p>
+          No account ? No problem,{" "}
+          <button className="signin-reset-button">Sign up</button>
+        </p>
       </div>
-      <p>
-        No account ? No problem,{" "}
-        <button className="signin-reset-button">Sign up</button>
-      </p>
     </div>
   );
 };
