c317dd7ad78b0824921aa59b10c9a0af2a5c16d9
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-19 17:52:16 +0200

    Finished the basic stying for signin

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,49 +1,97 @@
-import { Button, capitalize, TextField } from "@mui/material";
+import { Button, capitalize, Divider, TextField } from "@mui/material";
 import React from "react";
 import "./Signin.css";
 
 const Signin = () => {
   return (
-    <div className="signin-container bg-">
-      <div className="signin-center-content flex flex-col items-center justify-center w-3/5 m-auto">
-        <div className="flex">
-          <div className="signin-left-side">
-            <h3 className="">Welcome back!</h3>
-            <p>Let today be as a step to empower growth!</p>
+    <div className="signin-container">
+      <div className="flex items-center justify-center min-h-screen">
+        <div
+          className="bg-white w-[923px] p-12 pb-0 rounded-lg shadow-lg flex flex-col items-center justify-center gap-10"
+          style={{ borderRadius: 50 }}
+        >
+          <div className="flex items-center justify-center w-full ">
+            <div className="w-3/5 pr-8">
+              <div>
+                <h3 className="mb-2 text-2xl font-bold text-blue-600">
+                  Welcome Back!
+                </h3>
+                <p className="mb-6 text-sm text-gray-500">
+                  Let today be as a step to empower growth!
+                </p>
 
-            <div className="signin-form flex flex-col gap-2">
-              <label htmlFor="username" className="signin-label">
-                Username
-              </label>
-              <TextField id="username" variant="outlined" />
+                <div className="flex flex-col mb-4 space-y-4">
+                  <div>
+                    <label
+                      htmlFor="username"
+                      className="block mb-1 font-medium text-gray-600"
+                    >
+                      Username
+                    </label>
+                    <input
+                      type="text"
+                      id="username"
+                      className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      placeholder="Enter your username"
+                    />
+                  </div>
 
-              <label htmlFor="password" className="signin-label">
-                Password
-              </label>
-              <TextField id="username" type="password" variant="outlined" />
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block mb-1 font-medium text-gray-600"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      placeholder="Enter your password"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-between mb-4">
+                  <p className="text-sm text-gray-500">
+                    Forgot your password?{" "}
+                    <button className="text-blue-500 hover:underline">
+                      Reset
+                    </button>
+                  </p>
+                </div>
+
+                <button className="w-full py-3 text-lg font-extrabold text-white transition bg-blue-600 rounded-md hover:bg-blue-700">
+                  Sign in
+                </button>
+              </div>
             </div>
-            <p>
-              Forgot your password?{" "}
-              <button className="signin-reset-button">Reset</button>
-            </p>
-            <Button
-              variant="contained"
-              style={{
-                width: "100%",
-                fontWeight: "bold",
-                fontSize: 24,
-                textTransform: "none",
+            <Divider
+              orientation="vertical"
+              textAlign="center"
+              flexItem
+              sx={{
+                color: "#1E25A6",
+                "::before, ::after": {
+                  borderColor: "#1E25A6",
+                },
+                fontWeight: "800",
+                fontSize: 20,
               }}
             >
-              Sign in
-            </Button>
+              OR
+            </Divider>
+            <div className="flex flex-col items-center justify-center w-2/5">
+              <strong>Sign in with Google</strong>
+              <strong>Sign in with Github</strong>
+              <strong>Sign in with Linkedin</strong>
+            </div>
           </div>
-          <div className="signin-right-side"></div>
+          <p className="pb-4 text-sm text-gray-500">
+            No account? No problem,{" "}
+            <button className="text-blue-500 hover:underline">Signup</button>
+          </p>
         </div>
-        <p>
-          No account ? No problem,{" "}
-          <button className="signin-reset-button">Sign up</button>
-        </p>
       </div>
     </div>
   );
