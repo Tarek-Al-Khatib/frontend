b310afa747f9922d157553560708829e4b5d4310
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-19 03:52:10 +0200

    Added mui material UI component library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signin from "./pages/Signin/Signin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<div>Homepage</div>}></Route>
-        <Route path="/signin" element={<div>Sign in page</div>}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
         <Route path="/signup" element={<div>Sign up page</div>}></Route>
         <Route path="/learning" element={<div>My learning page</div>}></Route>
         <Route path="/myprofile" element={<div>My profile page</div>}></Route>
