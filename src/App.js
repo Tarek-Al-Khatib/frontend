73da7ba39e10afa63e03dccf9e1794ebb376743f
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-20 18:33:33 +0200

    Added Home component and added it's route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signin from "./pages/Signin/Signin";
 import SignUp from "./pages/Signup/Signup";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Homepage</div>}></Route>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Home />
+            </div>
+          }
+        ></Route>
         <Route path="/signin" element={<Signin />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/learning" element={<div>My learning page</div>}></Route>
