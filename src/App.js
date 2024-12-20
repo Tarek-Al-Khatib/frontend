0d6bfc9f2b9e3eb05c301246ab0c012c45bf53f1
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-21 00:11:29 +0200

    Added dashboard component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signin from "./pages/Signin/Signin";
 import SignUp from "./pages/Signup/Signup";
 import Home from "./pages/Home/Home";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   return (
@@ -12,11 +13,11 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/signin" element={<Signin />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/learning" element={<div>My learning page</div>}></Route>
         <Route path="/myprofile" element={<div>My profile page</div>}></Route>
         <Route path="/community" element={<div>communities page</div>}></Route>
         <Route path="/interview" element={<div>interviews page</div>}></Route>
-        <Route path="/dashboard" element={<div>Homepage</div>}></Route>
       </Routes>
     </BrowserRouter>
   );
