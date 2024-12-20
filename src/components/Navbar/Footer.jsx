9ae8159d374514e54b1491e6b4e2d3993c015872
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-21 00:37:14 +0200

    Added footer component and added it to dashboard

---
+++ src/components/Navbar/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../css/colors.css";
+const Footer = () => {
+  return (
+    <footer className="flex justify-between px-6 py-4 text-xl font-thin text-center bg-navy text-cyan">
+      <p>2024 &copy; WorkWise</p>
+      <p>Empower growth, unlock potential</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Navbar/Footer";
 
 const Dashboard = () => {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 };
