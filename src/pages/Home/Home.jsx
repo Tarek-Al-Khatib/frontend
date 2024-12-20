f9298fc09184522859fa38ef883148cd021eb291
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-20 19:36:22 +0200

    Added hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../css/colors.css";
+import "./Home.css";
 const Home = () => {
   return (
     <div className="bg-gray-100">
@@ -25,6 +26,26 @@ const Home = () => {
           </div>
         </div>
       </header>
+
+      <section className="flex text-center h-[700px]">
+        <div className="w-10/12 ">
+          <img
+            src="https://www.grosvenorsystems.com/hs-fs/hubfs/Optimized-ezgif.com-gif-maker-1.jpg?width=760&name=Optimized-ezgif.com-gif-maker-1.jpg"
+            alt="Logo"
+            className="object-cover w-full h-full"
+          />
+        </div>
+        <div className="flex flex-col justify-start w-3/5 p-28 gap-36">
+          <h1 className="text-6xl font-extrabold leading-normal tracking-widest text-navy text-start">
+            Let's start our careers from here
+          </h1>
+          <div>
+            <button className="block w-40 px-6 py-3 font-bold text-white rounded bg-navy hover:bg-blue-800">
+              Learn More
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
