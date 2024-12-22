09a80a45909a93a7f9ec3f373d4d1c95da06a260
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-22 18:17:25 +0200

    Created learning page component and routed it

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SignUp from "./pages/Signup/Signup";
 import Home from "./pages/Home/Home";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Communities from "./pages/Communities/Communities";
+import Learning from "./pages/Learning/Learning";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Route path="/signin" element={<Signin />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
-        <Route path="/learning" element={<div>My learning page</div>}></Route>
+        <Route path="/learning" element={<Learning />}></Route>
         <Route path="/myprofile" element={<div>My profile page</div>}></Route>
         <Route path="/community" element={<Communities />}></Route>
         <Route path="/interview" element={<div>interviews page</div>}></Route>
---
+++ src/pages/Learning/Learning.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Learning = () => {
+  return <div></div>;
+};
+
+export default Learning;
