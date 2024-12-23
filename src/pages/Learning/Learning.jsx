922325e2a3bd9ac35e2b12e5358d99f412c70305
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-24 01:09:34 +0200

    Added learning card with steps structure in learning page

--- src/pages/Learning/Learning.jsx
+++ src/pages/Learning/Learning.jsx
@@ -1,64 +1,316 @@
 import React from "react";
+import CircularWithValueLabel from "../../components/CircularProgressWithLabel/CircularProgressWithLabel";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import { Divider } from "@mui/material";
 
 const Learning = () => {
   return (
     <div>
       <Navbar />
-      <div class="min-h-screen bg-white p-8">
+      <div className="min-h-screen p-8 bg-white">
         <div className="flex justify-center">
           <div className="w-1/4">
-            <div class="text-navy text-2xl font-light mb-16">
+            <div className="mb-16 text-2xl font-light text-navy">
               My Learning | Learning Plans
             </div>
-            <div class="text-4xl font-bold mb-2 text-navy">Hey Nour!</div>
-            <div class="text-xl font-light mb-8 text-navy">
+            <div className="mb-2 text-4xl font-bold text-navy">Hey Nour!</div>
+            <div className="mb-8 text-xl font-light text-navy">
               Here you can check your learning plans and see each plan's steps
               and progress. Have a productive day!
             </div>
           </div>
 
           <div className="flex flex-col items-stretch flex-grow gap-5 pl-20 mt-10">
-            <div class="flex justify-end">
-              <button class="bg-navy text-white text-xl font-normal px-12 py-2 rounded-2xl">
+            <div className="flex justify-end">
+              <button className="px-12 py-2 text-xl font-normal text-white bg-navy rounded-2xl">
                 Add Plan
               </button>
             </div>
-            <div class="flex justify-center gap-10 mb-12 h-60">
-              <div class="flex items-center justify-center w-2/5 h-4/5  bg-white border rounded-xl border-gray-200 p-6">
-                <div class="w-28 h-28 bg-navy rounded-full flex items-center justify-center mb-4"></div>
+            <div className="flex justify-center gap-10 mb-12 h-60">
+              <div className="flex items-center justify-center w-2/5 p-6 bg-white border border-gray-200 h-4/5 rounded-xl">
+                <div className="flex items-center justify-center mb-4 rounded-full w-28 h-28 bg-navy"></div>
                 <div>
-                  <div class="text-navy text-6xl font-bold text-center mb-5">
+                  <div className="mb-5 text-6xl font-bold text-center text-navy">
                     13
                   </div>
-                  <div class="text-navy text-xl font-normal">Plans Created</div>
+                  <div className="text-xl font-normal text-navy">
+                    Plans Created
+                  </div>
                 </div>
               </div>
 
-              <div class="flex items-center justify-center w-2/5 h-4/5  bg-white rounded-xl border border-gray-200 p-6">
-                <div class="w-28 h-28 bg-navy rounded-full flex items-center justify-center mb-4"></div>
+              <div className="flex items-center justify-center w-2/5 p-6 bg-white border border-gray-200 h-4/5 rounded-xl">
+                <div className="flex items-center justify-center mb-4 rounded-full w-28 h-28 bg-navy"></div>
                 <div>
-                  <div class="text-navy text-5xl font-bold text-center mb-5  ">
+                  <div className="mb-5 text-5xl font-bold text-center text-navy ">
                     4
                   </div>
-                  <div class="text-navy text-xl font-normal">
+                  <div className="text-xl font-normal text-navy">
                     Plans Completed
                   </div>
                 </div>
               </div>
 
-              <div class="flex items-center h-4/5 justify-center w-2/5 bg-white rounded-xl border border-gray-200 p-6">
-                <div class="w-28 h-28 bg-navy rounded-full flex items-center justify-center mb-4"></div>
+              <div className="flex items-center justify-center w-2/5 p-6 bg-white border border-gray-200 h-4/5 rounded-xl">
+                <div className="flex items-center justify-center mb-4 rounded-full w-28 h-28 bg-navy"></div>
                 <div>
-                  <div class="text-navy text-5xl font-bold text-center mb-5  ">
+                  <div className="mb-5 text-5xl font-bold text-center text-navy ">
                     7
                   </div>
-                  <div class="text-navy text-xl font-normal">
+                  <div className="text-xl font-normal text-navy">
                     Steps in progress
                   </div>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className=" border rounded-2xl border-gray-200 bg-white p-4 w-full max-w-sm h-[600px]">
+            <div className="flex gap-6">
+              <div>
+                <div className="text-navy text-2xl font-bold font-['Open Sans Hebrew'] mb-4">
+                  Learning Plan
+                </div>
+                <div className="text-navy text-xl font-normal font-['Open Sans Hebrew'] mb-6">
+                  This is a learning plan that was implemented by the user
+                </div>
+              </div>
+              <div>
+                <CircularWithValueLabel value={67} size={60} />
+              </div>
+            </div>
+
+            <div className="relative p-4 bg-white ">
+              <div className="space-y-4 h-[400px] overflow-y-scroll">
+                <div className="flex items-center space-x-4">
+                  <img
+                    className="w-14 h-14"
+                    src="https://via.placeholder.com/61x59"
+                    alt="Step Icon"
+                  />
+                  <div>
+                    <div className="text-base font-normal text-navy">Step</div>
+                    <Divider
+                      orientation="horizontal"
+                      textAlign="center"
+                      flexItem
+                      sx={{
+                        color: "#1E25A6",
+                        "::before, ::after": {
+                          borderColor: "#1E25A6",
+                        },
+                        fontWeight: "800",
+                        fontSize: 20,
+                      }}
+                    ></Divider>
+                    <div className="text-2xl font-normal text-navy">
+                      Do something
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <img
+                    className="w-14 h-14"
+                    src="https://via.placeholder.com/61x59"
+                    alt="Step Icon"
+                  />
+                  <div>
+                    <div className="text-base font-normal text-navy">Step</div>
+                    <Divider
+                      orientation="horizontal"
+                      textAlign="center"
+                      flexItem
+                      sx={{
+                        color: "#1E25A6",
+                        "::before, ::after": {
+                          borderColor: "#1E25A6",
+                        },
+                        fontWeight: "800",
+                        fontSize: 20,
+                      }}
+                    ></Divider>
+                    <div className="text-2xl font-normal text-navy">
+                      Do something
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <img
+                    className="w-14 h-14"
+                    src="https://via.placeholder.com/61x59"
+                    alt="Step Icon"
+                  />
+                  <div>
+                    <div className="text-base font-normal text-navy">Step</div>
+                    <Divider
+                      orientation="horizontal"
+                      textAlign="center"
+                      flexItem
+                      sx={{
+                        color: "#1E25A6",
+                        "::before, ::after": {
+                          borderColor: "#1E25A6",
+                        },
+                        fontWeight: "800",
+                        fontSize: 20,
+                      }}
+                    ></Divider>
+                    <div className="text-2xl font-normal text-navy">
+                      Do something
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <img
+                    className="w-14 h-14"
+                    src="https://via.placeholder.com/61x59"
+                    alt="Step Icon"
+                  />
+                  <div>
+                    <div className="text-base font-normal text-navy">Step</div>
+                    <Divider
+                      orientation="horizontal"
+                      textAlign="center"
+                      flexItem
+                      sx={{
+                        color: "#1E25A6",
+                        "::before, ::after": {
+                          borderColor: "#1E25A6",
+                        },
+                        fontWeight: "800",
+                        fontSize: 20,
+                      }}
+                    ></Divider>
+                    <div className="text-2xl font-normal text-navy">
+                      Do something
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <img
+                    className="w-14 h-14"
+                    src="https://via.placeholder.com/61x59"
+                    alt="Step Icon"
+                  />
+                  <div>
+                    <div className="text-base font-normal text-navy">Step</div>
+                    <Divider
+                      orientation="horizontal"
+                      textAlign="center"
+                      flexItem
+                      sx={{
+                        color: "#1E25A6",
+                        "::before, ::after": {
+                          borderColor: "#1E25A6",
+                        },
+                        fontWeight: "800",
+                        fontSize: 20,
+                      }}
+                    ></Divider>
+                    <div className="text-2xl font-normal text-navy">
+                      Do something
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex items-center space-x-4">
+                  <img
+                    className="w-14 h-14"
+                    src="https://via.placeholder.com/61x59"
+                    alt="Step Icon"
+                  />
+                  <div>
+                    <div className="text-base font-normal text-navy">Step</div>
+                    <Divider
+                      orientation="horizontal"
+                      textAlign="center"
+                      flexItem
+                      sx={{
+                        color: "#1E25A6",
+                        "::before, ::after": {
+                          borderColor: "#1E25A6",
+                        },
+                        fontWeight: "800",
+                        fontSize: 20,
+                      }}
+                    ></Divider>
+                    <div className="text-2xl font-normal text-navy">
+                      Do something
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <img
+                    className="w-14 h-14"
+                    src="https://via.placeholder.com/61x59"
+                    alt="Step Icon"
+                  />
+                  <div>
+                    <div className="text-base font-normal text-navy">Step</div>
+                    <Divider
+                      orientation="horizontal"
+                      textAlign="center"
+                      flexItem
+                      sx={{
+                        color: "#1E25A6",
+                        "::before, ::after": {
+                          borderColor: "#1E25A6",
+                        },
+                        fontWeight: "800",
+                        fontSize: 20,
+                      }}
+                    ></Divider>
+                    <div className="text-2xl font-normal text-navy">
+                      Do something
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex items-center space-x-4">
+                  <img
+                    className="w-14 h-14"
+                    src="https://via.placeholder.com/61x59"
+                    alt="Step Icon"
+                  />
+                  <div>
+                    <div className="text-base font-normal text-navy">Step</div>
+                    <div className="text-2xl font-normal text-navy">
+                      Do something
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <img
+                    className="w-14 h-14"
+                    src="https://via.placeholder.com/61x59"
+                    alt="Step Icon"
+                  />
+                  <div>
+                    <div className="text-base font-normal text-navy">Step</div>
+                    <div className="text-2xl font-normal text-navy">
+                      Do something
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <img
+                    className="w-14 h-14"
+                    src="https://via.placeholder.com/61x59"
+                    alt="Step Icon"
+                  />
+                  <div>
+                    <div className="text-base font-normal text-navy">Step</div>
+                    <div className="text-2xl font-normal text-navy">
+                      Do something
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="absolute top-0 left-0 right-0 h-16 pointer-events-none bg-gradient-to-b from-white via-white/70 to-transparent"></div>
+              <div className="absolute bottom-0 left-0 right-0 h-16 pointer-events-none bg-gradient-to-t from-white via-white/70 to-transparent"></div>
             </div>
           </div>
         </div>
