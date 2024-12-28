3f38328347d9977a8f2c887cf3e35a62d90eeb94
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-28 20:14:59 +0200

    Used app provider in app.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,24 @@ import Home from "./pages/Home/Home";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Communities from "./pages/Communities/Communities";
 import Learning from "./pages/Learning/Learning";
+import AppProviders from "./contexts/AppProviders";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/signin" element={<Signin />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
-        <Route path="/learning" element={<Learning />}></Route>
-        <Route path="/myprofile" element={<div>My profile page</div>}></Route>
-        <Route path="/community" element={<Communities />}></Route>
-        <Route path="/interview" element={<div>interviews page</div>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <AppProviders>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/learning" element={<Learning />}></Route>
+          <Route path="/myprofile" element={<div>My profile page</div>}></Route>
+          <Route path="/community" element={<Communities />}></Route>
+          <Route path="/interview" element={<div>interviews page</div>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </AppProviders>
   );
 }
 
