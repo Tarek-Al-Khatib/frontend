32f17735209f65e1101704eb15ea1e731531f457
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-28 21:54:44 +0200

    Updated the page with mapping communities

--- src/pages/Communities/Communities.jsx
+++ src/pages/Communities/Communities.jsx
@@ -85,10 +85,15 @@ const Communities = () => {
       <Navbar />
       <div class="flex bg-white w-full h-screen/92">
         <div class="w-24 bg-white flex flex-col items-center py-6">
-          <button class="w-20 h-20 bg-navy rounded-full mb-5"></button>
-          <button class="w-20 h-20 bg-navy rounded-full mb-5"></button>
-          <button class="w-20 h-20 bg-navy rounded-full mb-5"></button>
-          <button class="w-20 h-20 bg-navy rounded-full mb-5"></button>
+          {communitiesData.map((community) => (
+            <button
+              key={community.id}
+              className="w-20 h-20 mb-5 rounded-full bg-navy"
+              onClick={() => handleCommunitySelect(community)}
+            >
+              <span className="text-white">{community.logo}</span>
+            </button>
+          ))}
           <button class="w-12 h-12 bg-white rounded-full flex items-center justify-center shadow-lg">
             <IoMdAdd size={25} />
           </button>
