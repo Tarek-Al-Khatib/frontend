919ea7acfb2841058f13b52a32e2152a64568280
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-28 20:12:15 +0200

    Added fetchSignup

--- src/contexts/AuthContext/AuthContext.js
+++ src/contexts/AuthContext/AuthContext.js
@@ -31,7 +31,28 @@ const AuthProvider = ({ children }) => {
     }
   };
 
-  const fetchSignup = async () => {};
+  const fetchSignup = async (username, email, password) => {
+    try {
+      const response = await axios.post(`${serverUrl}/api/auth/register`, {
+        username,
+        email,
+        password,
+      });
+      const data = response.data;
+
+      setUser(data.user);
+      setToken(data.token);
+
+      localStorage.setItem("authToken", token);
+
+      return user;
+    } catch (error) {
+      console.error("Error during signup:", error);
+      throw new Error(
+        error.response?.data?.error || "Failed to sign up. Please try again."
+      );
+    }
+  };
 
   return (
     <authContext.Provider
