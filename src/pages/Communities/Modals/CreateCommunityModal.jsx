5a075c9e2e21ea9035f35f4d869c964e7dfa9859
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-30 05:44:42 +0200

    Added basic code for community creation modal

---
+++ src/pages/Communities/Modals/CreateCommunityModal.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const CreateCommunity = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    logo: null,
+    banner: null,
+  });
+  return <div></div>;
+};
+
+export default CreateCommunity;
