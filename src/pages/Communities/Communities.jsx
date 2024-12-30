58ae5e79bec0bfd88d186726b3e66ad11b65f7eb
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-30 04:18:06 +0200

    Added selecting a channel

--- src/pages/Communities/Communities.jsx
+++ src/pages/Communities/Communities.jsx
@@ -20,13 +20,13 @@ const Communities = () => {
   } = useContext(communityContext);
 
   const messagesContainerRef = useRef(null);
-
   const [messageInput, setMessageInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
   const [selectedCommunity, setSelectedCommunity] = useState(
     communities[0] || null
   );
   const [selectedChannel, setSelectedChannel] = useState(null);
-  const [messages, setMessages] = useState([]);
   const [moderatorsData, setModeratorsData] = useState(
     members.filter((m) => m.role === "MODERATOR")
   );
@@ -65,7 +65,7 @@ const Communities = () => {
     }
   };
 
-  const messagesToRender = renderMessages();
+  const messagesToRender = renderMessages(messages);
 
   return (
     <div>
@@ -141,76 +141,84 @@ const Communities = () => {
             </div>
           </div>
         </div>
-
-        <div className="flex flex-col flex-grow gap-4 p-6 bg-white">
-          <div>
-            <h1 className="mb-4 text-4xl text-navy">Welcome to //general</h1>
-            <p className="mb-4 text-xl font-thin text-navy">
-              @Admin created this channel on 17/07/2024: Channel description
-              (not more than 250 chars)
-            </p>
-            <hr className="mb-6 border-t border-blue-900" />
-          </div>
-          <div
-            ref={messagesContainerRef}
-            className="flex flex-col flex-grow gap-5 overflow-y-auto scroll-container custom-scrollbar scroll-smooth"
-          >
-            <div className="flex flex-col flex-grow gap-5 ">
-              {messagesToRender.length > 0 ? (
-                messagesToRender.map((message, index) => (
-                  <div key={index}>
-                    <div className="flex items-start gap-7">
-                      <div className="w-12 h-12 bg-blue-900 rounded-full"></div>
-                      <div className="flex-1">
-                        <div className="flex items-center justify-between gap-6 w-fit">
-                          <div className="text-sm font-bold text-blue-900">
-                            {message.name}
+        {selectedChannel ? (
+          <div className="flex flex-col flex-grow gap-4 p-6 bg-white">
+            <div>
+              <h1 className="mb-4 text-4xl text-navy">Welcome to //general</h1>
+              <p className="mb-4 text-xl font-thin text-navy">
+                @Admin created this channel on 17/07/2024: Channel description
+                (not more than 250 chars)
+              </p>
+              <hr className="mb-6 border-t border-blue-900" />
+            </div>
+            <div
+              ref={messagesContainerRef}
+              className="flex flex-col flex-grow gap-5 overflow-y-auto scroll-container custom-scrollbar scroll-smooth"
+            >
+              <div className="flex flex-col flex-grow gap-5 ">
+                {messagesToRender.length > 0 ? (
+                  messagesToRender.map((message, index) => (
+                    <div key={index}>
+                      <div className="flex items-start gap-7">
+                        <div className="w-12 h-12 bg-blue-900 rounded-full"></div>
+                        <div className="flex-1">
+                          <div className="flex items-center justify-between gap-6 w-fit">
+                            <div className="text-sm font-bold text-blue-900">
+                              {message.name}
+                            </div>
+                            <div className="px-2 py-1 text-xs text-white bg-blue-900 rounded">
+                              {message.badge}
+                            </div>
+                            <div className="text-sm text-blue-900">
+                              {message.timestamp}
+                            </div>
                           </div>
-                          <div className="px-2 py-1 text-xs text-white bg-blue-900 rounded">
-                            {message.badge}
-                          </div>
-                          <div className="text-sm text-blue-900">
-                            {message.timestamp}
-                          </div>
+                          <p
+                            className="text-base text-black"
+                            dangerouslySetInnerHTML={{
+                              __html: message.message,
+                            }}
+                          ></p>
                         </div>
-                        <p
-                          className="text-base text-black"
-                          dangerouslySetInnerHTML={{ __html: message.message }}
-                        ></p>
                       </div>
                     </div>
-                  </div>
-                ))
-              ) : (
-                <div>No Messages to show here</div>
-              )}
+                  ))
+                ) : (
+                  <div>No Messages to show here</div>
+                )}
+              </div>
+            </div>
+            <div className="flex items-center gap-6 p-4 bg-slight-gray rounded-self">
+              <button className="flex items-center justify-center w-12 h-12 bg-white rounded-full shadow-lg">
+                <IoMdAdd size={25} className="text-navy" />
+              </button>
+              <input
+                type="text"
+                className="flex-1 text-xl placeholder-gray-400 bg-transparent outline-none"
+                placeholder="Type your message here for //general"
+                value={messageInput}
+                onChange={(e) => setMessageInput(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    e.preventDefault();
+                    handleMessageSend();
+                  }
+                }}
+              />
+              <button
+                className="flex items-center justify-center w-12 h-12 bg-white rounded-full shadow-lg"
+                onClick={handleMessageSend}
+              >
+                <FiSend size={25} className="text-navy" />
+              </button>
             </div>
           </div>
-          <div className="flex items-center gap-6 p-4 bg-slight-gray rounded-self">
-            <button className="flex items-center justify-center w-12 h-12 bg-white rounded-full shadow-lg">
-              <IoMdAdd size={25} className="text-navy" />
-            </button>
-            <input
-              type="text"
-              className="flex-1 text-xl placeholder-gray-400 bg-transparent outline-none"
-              placeholder="Type your message here for //general"
-              value={messageInput}
-              onChange={(e) => setMessageInput(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  e.preventDefault();
-                  handleMessageSend();
-                }
-              }}
-            />
-            <button
-              className="flex items-center justify-center w-12 h-12 bg-white rounded-full shadow-lg"
-              onClick={handleMessageSend}
-            >
-              <FiSend size={25} className="text-navy" />
-            </button>
+        ) : (
+          <div className="flex items-center justify-center flex-grow text-2xl font-bold text-gray-500">
+            Please select a channel to start chatting.
           </div>
-        </div>
+        )}
+        }
       </div>
       <Footer />
     </div>
