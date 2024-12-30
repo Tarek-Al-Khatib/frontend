d8c06e1822aafa276eb2a5651ed6b636491dcfab
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-30 21:46:25 +0200

    Added dotenv and implemented the linking to quotes api

--- src/contexts/DashboardContext/DashboardContext.js
+++ src/contexts/DashboardContext/DashboardContext.js
@@ -1,11 +1,31 @@
 import React, { createContext, useContext, useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
+import dotenv from "dotenv";
 
 export const dashboardContext = createContext();
+dotenv.config();
 
 const DashboardProvider = ({ children }) => {
-  return <dashboardContext.Provider></dashboardContext.Provider>;
+  const getQuote = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.api-ninjas.com/v1/quotes?category=success",
+        {
+          headers: {
+            "X-Api-Key": process.env.NINJAS_API,
+          },
+        }
+      );
+
+      console.log(response.data);
+    } catch (e) {
+      console.log("Error getting a quote:", e);
+    }
+  };
+  return (
+    <dashboardContext.Provider value={getQuote}></dashboardContext.Provider>
+  );
 };
 
 export default DashboardProvider;
