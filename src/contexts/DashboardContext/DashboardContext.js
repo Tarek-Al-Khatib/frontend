ea45e2a0d2ccd5ae3b234d8e0515a1fcba623e0f
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-30 22:09:14 +0200

    Fixed getting the quote from dashboard context

--- src/contexts/DashboardContext/DashboardContext.js
+++ src/contexts/DashboardContext/DashboardContext.js
@@ -1,30 +1,39 @@
-import React, { createContext, useContext, useEffect } from "react";
-import { useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
-import dotenv from "dotenv";
 
 export const dashboardContext = createContext();
-dotenv.config();
 
 const DashboardProvider = ({ children }) => {
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      await getQuote();
+    };
+
+    fetchQuote();
+  }, []);
+
   const getQuote = async () => {
     try {
       const response = await axios.get(
         "https://api.api-ninjas.com/v1/quotes?category=success",
         {
           headers: {
-            "X-Api-Key": process.env.NINJAS_API,
+            "X-Api-Key": process.env.REACT_APP_NINJAS_API,
           },
         }
       );
 
-      console.log(response.data);
+      setQuote(response.data[0]);
     } catch (e) {
       console.log("Error getting a quote:", e);
     }
   };
   return (
-    <dashboardContext.Provider value={getQuote}></dashboardContext.Provider>
+    <dashboardContext.Provider value={{ quote }}>
+      {children}
+    </dashboardContext.Provider>
   );
 };
 
