8cff7481ff7996058aee393aaafb0171daaae8ab
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-02 22:21:54 +0200

    Added my learning plans html structure and styling to profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -10,6 +10,7 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
+import CircularProgressWithLabel from "../../components/CircularProgressWithLabel/CircularProgressWithLabel";
 
 const Profile = () => {
   const leaderboardData = [
@@ -51,55 +52,140 @@ const Profile = () => {
   ];
   return (
     <div className="flex">
-      <div class="flex gap-5 justify-between w-[650px] bg-white rounded-[15px] p-6">
-        <div class="w-32 h-32 bg-[#1e25a6] rounded-full flex justify-center items-center">
-          <span class="text-white text-sm font-bold font-['Open Sans Hebrew']">
-            user img
-          </span>
-        </div>
-
-        <div class="flex justify-center gap-10">
-          <div className="flex flex-col justify-center w-1/3 gap-5">
-            <div class="flex justify-start gap-5">
-              <span class="text-[#1e25a6] text-sm font-normal font-['Open Sans Hebrew']">
-                Name:
-              </span>
-              <span class="text-[#1e25a6] text-sm font-bold font-['Open Sans Hebrew']">
-                Username
-              </span>
-            </div>
-            <div class="flex justify-start gap-5">
-              <span class="text-[#1e25a6] text-sm font-normal font-['Open Sans Hebrew']">
-                Email:
-              </span>
-              <span class="text-[#1e25a6] text-sm font-bold font-['Open Sans Hebrew']">
-                Email
-              </span>
-            </div>
-            <div class="flex justify-start gap-5">
-              <span class="text-[#1e25a6] text-sm font-normal font-['Open Sans Hebrew']">
-                Points:
-              </span>
-              <span class="text-[#1e25a6] text-sm font-bold font-['Open Sans Hebrew']">
-                1200
-              </span>
-            </div>
-          </div>
-          <div class="flex flex-col items-center w-1/3">
-            <span class="text-[#1e25a6] text-base font-extrabold font-['Open Sans Hebrew'] mb-4">
-              Badges
+      <div>
+        <div class="flex gap-5 justify-between w-[650px] bg-white rounded-[15px] p-6 h-fit">
+          <div class="w-32 h-32 bg-[#1e25a6] rounded-full flex justify-center items-center">
+            <span class="text-white text-sm font-bold font-['Open Sans Hebrew']">
+              user img
             </span>
-            <div class="flex flex-wrap gap-3 justify-center">
-              <div class="w-9 h-9 bg-[#1e25a6] rounded-full flex justify-center items-center">
-                <span class="text-black text-base font-extrabold font-['Open Sans Hebrew']">
-                  13
+          </div>
+
+          <div class="flex justify-center gap-10">
+            <div className="flex flex-col justify-center w-1/3 gap-5">
+              <div class="flex justify-start gap-5">
+                <span class="text-[#1e25a6] text-sm font-normal font-['Open Sans Hebrew']">
+                  Name:
+                </span>
+                <span class="text-[#1e25a6] text-sm font-bold font-['Open Sans Hebrew']">
+                  Username
                 </span>
               </div>
-              <div class="w-9 h-9 bg-[#1e25a6] rounded-full"></div>
-              <div class="w-9 h-9 bg-[#1e25a6] rounded-full"></div>
-              <div class="w-9 h-9 bg-[#1e25a6] rounded-full"></div>
-              <div class="w-9 h-9 bg-[#1e25a6] rounded-full"></div>
-              <div class="w-9 h-9 bg-[#1e25a6] rounded-full"></div>
+              <div class="flex justify-start gap-5">
+                <span class="text-[#1e25a6] text-sm font-normal font-['Open Sans Hebrew']">
+                  Email:
+                </span>
+                <span class="text-[#1e25a6] text-sm font-bold font-['Open Sans Hebrew']">
+                  Email
+                </span>
+              </div>
+              <div class="flex justify-start gap-5">
+                <span class="text-[#1e25a6] text-sm font-normal font-['Open Sans Hebrew']">
+                  Points:
+                </span>
+                <span class="text-[#1e25a6] text-sm font-bold font-['Open Sans Hebrew']">
+                  1200
+                </span>
+              </div>
+            </div>
+            <div class="flex flex-col items-center w-1/3">
+              <span class="text-[#1e25a6] text-base font-extrabold font-['Open Sans Hebrew'] mb-4">
+                Badges
+              </span>
+              <div class="flex flex-wrap gap-3 justify-center">
+                <div class="w-9 h-9 bg-[#1e25a6] rounded-full flex justify-center items-center">
+                  <span class="text-black text-base font-extrabold font-['Open Sans Hebrew']">
+                    13
+                  </span>
+                </div>
+                <div class="w-9 h-9 bg-[#1e25a6] rounded-full"></div>
+                <div class="w-9 h-9 bg-[#1e25a6] rounded-full"></div>
+                <div class="w-9 h-9 bg-[#1e25a6] rounded-full"></div>
+                <div class="w-9 h-9 bg-[#1e25a6] rounded-full"></div>
+                <div class="w-9 h-9 bg-[#1e25a6] rounded-full"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="border rounded-2xl border-gray-200 bg-white p-4 w-full max-w-sm h-[650px]">
+          <div className="mb-3 text-base font-bold text-center text-navy">
+            My learning plans
+          </div>
+
+          <div className="p-4 bg-white h-[650px]">
+            <div className="space-y-4 h-[550px] overflow-y-auto overflow-x-hidden custom-scrollbar whitespace-normal">
+              <div className="flex justify-between px-5">
+                <div className="flex flex-col gap-2">
+                  <p className="font-bold text-navy">Learning plan</p>
+                  <p className="font-normal text-navy">description</p>
+                </div>
+                <div>
+                  <CircularProgressWithLabel value={67} size={60} />
+                </div>
+              </div>
+              <div className="flex justify-between px-5">
+                <div className="flex flex-col gap-2">
+                  <p className="font-bold text-navy">Learning plan</p>
+                  <p className="font-normal text-navy">description</p>
+                </div>
+                <div>
+                  <CircularProgressWithLabel value={67} size={60} />
+                </div>
+              </div>
+              <div className="flex justify-between px-5">
+                <div className="flex flex-col gap-2">
+                  <p className="font-bold text-navy">Learning plan</p>
+                  <p className="font-normal text-navy">description</p>
+                </div>
+                <div>
+                  <CircularProgressWithLabel value={67} size={60} />
+                </div>
+              </div>
+              <div className="flex justify-between px-5">
+                <div className="flex flex-col gap-2">
+                  <p className="font-bold text-navy">Learning plan</p>
+                  <p className="font-normal text-navy">description</p>
+                </div>
+                <div>
+                  <CircularProgressWithLabel value={67} size={60} />
+                </div>
+              </div>
+              <div className="flex justify-between px-5">
+                <div className="flex flex-col gap-2">
+                  <p className="font-bold text-navy">Learning plan</p>
+                  <p className="font-normal text-navy">description</p>
+                </div>
+                <div>
+                  <CircularProgressWithLabel value={67} size={60} />
+                </div>
+              </div>
+              <div className="flex justify-between px-5">
+                <div className="flex flex-col gap-2">
+                  <p className="font-bold text-navy">Learning plan</p>
+                  <p className="font-normal text-navy">description</p>
+                </div>
+                <div>
+                  <CircularProgressWithLabel value={67} size={60} />
+                </div>
+              </div>
+              <div className="flex justify-between px-5">
+                <div className="flex flex-col gap-2">
+                  <p className="font-bold text-navy">Learning plan</p>
+                  <p className="font-normal text-navy">description</p>
+                </div>
+                <div>
+                  <CircularProgressWithLabel value={67} size={60} />
+                </div>
+              </div>
+
+              <div className="flex justify-between px-5">
+                <div className="flex flex-col gap-2">
+                  <p className="font-bold text-navy">Learning plan</p>
+                  <p className="font-normal text-navy">description</p>
+                </div>
+                <div>
+                  <CircularProgressWithLabel value={67} size={60} />
+                </div>
+              </div>
             </div>
           </div>
         </div>
