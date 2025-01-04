381cbf674a8b193db36a8358a0b9dd4d3f5fa041
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-04 14:50:22 +0200

    Added navbar and footer to the interviews page

--- src/pages/Interviews/Interviews.jsx
+++ src/pages/Interviews/Interviews.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 
 const Interview = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Interview;
