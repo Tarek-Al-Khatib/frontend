7bdf47b1dcae7a73f9e7ae8f9a80e83e200259a3
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-04 21:29:08 +0200

    Added modal to see feedback

--- src/pages/Interviews/Modal/ViewFeedback.js
+++ src/pages/Interviews/Modal/ViewFeedback.js
@@ -1,7 +1,35 @@
+import { Box, Modal, Typography } from "@mui/material";
 import React from "react";
 
-const ViewFeedback = () => {
-  return <div></div>;
+const ViewFeedback = ({ text, open, handleClose }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 300,
+          bgcolor: "background.paper",
+          border: "2px solid #000",
+          boxShadow: 24,
+          p: 4,
+          textAlign: "center",
+          borderRadius: 2,
+        }}
+      >
+        <Typography id="simple-modal-title" variant="h6" component="h2">
+          {text}
+        </Typography>
+      </Box>
+    </Modal>
+  );
 };
 
 export default ViewFeedback;
