623ff88fd2d98eaa3efc32dc339f56775a81bfe9
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-04 20:55:54 +0200

    Added interview history section to interviews page

--- src/pages/Interviews/Interviews.jsx
+++ src/pages/Interviews/Interviews.jsx
@@ -1,18 +1,55 @@
 import React from "react";
+import {
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import TableCellStyled from "../../components/TableCell/TableCell";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import "../../css/colors.css";
 const Interview = () => {
+  const leaderboardData = [
+    {
+      rank: 1,
+      interviewer: "Username1",
+      learningPlans: 0,
+      points: 1200,
+    },
+    {
+      rank: 2,
+      interviewer: "Username2",
+
+      learningPlans: 0,
+      points: 1100,
+    },
+    {
+      rank: 3,
+      interviewer: "Username3",
+
+      learningPlans: 0,
+      points: 1000,
+    },
+    {
+      rank: 4,
+      interviewer: "Username4",
+
+      learningPlans: 0,
+      points: 950,
+    },
+  ];
   return (
     <div>
       <Navbar />
       <div class="p-8 min-h-screen">
-        <h1 className="pb-5 text-2xl font-thin text-navy">My Interviews</h1>
+        <h1 className="pb-5 text-2xl font-thin text-navy">My interviewer</h1>
 
         <div className="flex">
           <div class="bg-gradient-to-r from-[#b0e9ff] to-[#0594b4] rounded-l-lg w-full flex flex-col items-start justify-center p-6 pl-32 gap-4">
             <h1 class="text-navy text-4xl font-extrabold">
-              Practicing Interviews?
+              Practicing interviewer?
             </h1>
             <p class="text-navy text-2xl font-normal">
               Try out our new advanced{" "}
@@ -26,8 +63,8 @@ const Interview = () => {
               Just like a normal one!
               <span className="font-extrabold"> TRY IT NOW</span>
             </p>
-            <div class="flex flex-col items-center gap-4">
-              <button class="bg-[#1e25a5] text-white text-xl font-bold font-['Open_Sans_Hebrew'] px-6 py-3 rounded-md">
+            <div className="pl-16">
+              <button class="bg-[#1e25a5] text-white text-xl font-bold px-6 py-3 rounded-md">
                 Start Mock Interview
               </button>
             </div>
@@ -38,7 +75,47 @@ const Interview = () => {
             alt="AI Recruitment"
           />
         </div>
+        <div className="flex gap-20 py-20">
+          <div className="w-1/2">
+            <h1 className="pb-5 text-2xl font-normal text-navy">
+              Interview History
+            </h1>
+            <TableContainer className="p-3 pb-8 border border-gray-200 rounded-xl">
+              <Table aria-label="leaderboard table">
+                <TableHead>
+                  <TableRow>
+                    <TableCellStyled start={true}>Interviewer</TableCellStyled>
+                    <TableCellStyled>Feedback</TableCellStyled>
+                    <TableCellStyled>Status</TableCellStyled>
+                    <TableCellStyled end={true}>Points</TableCellStyled>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {leaderboardData.map((user) => (
+                    <TableRow key={user.rank}>
+                      <TableCellStyled start={true}>
+                        <div className="flex items-center gap-3">
+                          {user.rank} {". "}
+                          <div className="flex items-center gap-1">
+                            <div class="w-9 h-9 bg-[#1e25a6] rounded-full"></div>{" "}
+                            {user.interviewer}
+                          </div>
+                        </div>
+                      </TableCellStyled>
+                      <TableCellStyled>hi</TableCellStyled>
+                      <TableCellStyled>{user.learningPlans}</TableCellStyled>
+                      <TableCellStyled end={true}>
+                        +{user.points}
+                      </TableCellStyled>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        </div>
       </div>
+
       <Footer />
     </div>
   );
