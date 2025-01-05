6e767b63414e182c1d66b30ba04a4b84a31285b9
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-05 21:39:04 +0200

    Added mark as read notification to general context

--- src/contexts/GeneralContext/GeneralContext.js
+++ src/contexts/GeneralContext/GeneralContext.js
@@ -26,8 +26,29 @@ const GeneralProvider = ({ children }) => {
     setNotifications(response.data);
   };
 
+  const markAsReadNotifications = () => {
+    notifications
+      .filter((n) => !n.is_read)
+      .map(async (notification) => {
+        const response = await axios.patch(
+          `${serverUrl}/api/notifications/${notification.id}/read`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        console.log(response.data);
+      });
+
+    fetchNotifications(token);
+  };
+
   return (
-    <generalContext.Provider value={{ notifications, setNotifications }}>
+    <generalContext.Provider
+      value={{ notifications, setNotifications, markAsReadNotifications }}
+    >
       {children}
     </generalContext.Provider>
   );
