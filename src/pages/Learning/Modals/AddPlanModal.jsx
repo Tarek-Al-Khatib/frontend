a1a53ba32352aa32dcfa80ad83a22f293d72c099
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-06 05:12:22 +0200

    Added steps section to add learn plan modal

--- src/pages/Learning/Modals/AddPlanModal.jsx
+++ src/pages/Learning/Modals/AddPlanModal.jsx
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
-import { Box, Button, Divider, Modal, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  Modal,
+  TextField,
+} from "@mui/material";
 import "../../../css/colors.css";
 import { IoCloseCircleOutline } from "react-icons/io5";
+import {
+  IoIosAddCircleOutline,
+  IoIosRemoveCircleOutline,
+} from "react-icons/io";
 
 const LearningPlanModal = ({ open, handleClose }) => {
   const [learningPlan, setLearningPlan] = useState({
@@ -111,6 +122,99 @@ const LearningPlanModal = ({ open, handleClose }) => {
               }}
             />
           </div>
+        </div>
+        <h2 className="mb-1 text-3xl font-extrabold text-navy">Steps</h2>
+        <Divider
+          textAlign="center"
+          flexItem
+          sx={{
+            borderBottomColor: "#1E25A6",
+            marginBottom: 2,
+          }}
+        ></Divider>
+        <div className="flex items-center gap-5 mb-4">
+          <button onClick={addStep}>
+            {" "}
+            <IoIosAddCircleOutline className="text-navy" size={40} />
+          </button>
+          <button className="px-8 py-2 text-xl font-bold transition text-cyan bg-navy rounded- hover:bg-blue-800 rounded-self">
+            Enhance with AI
+          </button>
+        </div>
+        <div className="overflow-y-auto custom-scrollbar h-[300px] mb-5 scroll-smooth">
+          {steps.map((step, index) => (
+            <div
+              key={index}
+              className="flex items-center pb-2 mt-1 mb-4 border-b "
+            >
+              <div className="flex w-11/12 gap-10">
+                <div className="flex items-center justify-between w-1/3">
+                  <label
+                    htmlFor={`step-title-${index}`}
+                    className="text-2xl font-bold text-navy"
+                  >
+                    Title:
+                  </label>
+                  <TextField
+                    id="plan-title"
+                    name="plan-title"
+                    variant="outlined"
+                    placeholder="Learning title... ex: State NodeJS"
+                    onChange={handleInputChange}
+                    sx={{
+                      width: "80%",
+                      "& .MuiOutlinedInput-root": {
+                        fontFamily: "Open Sans",
+                        fontWeight: "700",
+                        borderRadius: 5,
+                      },
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#E5E5E5",
+                      },
+                    }}
+                  />
+                </div>
+                <div className="flex items-center justify-between w-3/5">
+                  <label
+                    htmlFor={`step-description-${index}`}
+                    className="text-2xl font-bold text-navy"
+                  >
+                    Description:
+                  </label>
+                  <TextField
+                    id={`step-description-${index}`}
+                    name={`step-description-${index}`}
+                    multiline={true}
+                    maxRows={4}
+                    placeholder="Learning description... ex: I will start by learning the fundamentals of NodeJS and then dive deep to become a professional NodeJS developer !"
+                    variant="outlined"
+                    onChange={handleInputChange}
+                    sx={{
+                      width: "80%",
+                      "& .MuiOutlinedInput-root": {
+                        fontFamily: "Open Sans",
+                        fontWeight: "700",
+                        borderRadius: 5,
+                      },
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#E5E5E5",
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+              <IconButton
+                color="error"
+                onClick={() => removeStep(index)}
+                disabled={steps.length === 1}
+              >
+                <IoIosRemoveCircleOutline
+                  size={40}
+                  className={steps.length === 1 ? "text-gray-400" : "text-navy"}
+                />
+              </IconButton>
+            </div>
+          ))}
         </div>
       </Box>
     </Modal>
