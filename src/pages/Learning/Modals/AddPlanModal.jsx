65c4d962a7e0b6943217b257951f5af607f07f17
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-06 05:08:32 +0200

    Added states for add modal

--- src/pages/Learning/Modals/AddPlanModal.jsx
+++ src/pages/Learning/Modals/AddPlanModal.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Modal } from "@mui/material";
 import "../../../css/colors.css";
 
 const LearningPlanModal = ({ open, handleClose }) => {
+  const [learningPlan, setLearningPlan] = useState({
+    title: "",
+    description: "",
+  });
+
+  const [steps, setSteps] = useState([
+    {
+      title: "",
+      description: "",
+    },
+  ]);
   return <Modal open={open} onClose={handleClose}></Modal>;
 };
 
