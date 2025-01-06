1f0cec09260f3a2768ed562204db6838b6774070
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-06 02:09:36 +0200

    Added fetchInterviews to interview context

--- src/contexts/InterviewContext/InterviewContext.js
+++ src/contexts/InterviewContext/InterviewContext.js
@@ -7,6 +7,22 @@ import { authContext } from "../AuthContext/AuthContext";
 export const interviewContext = createContext();
 
 const InterviewProvider = ({ children }) => {
+  const [userInterviews, setUserInterviews] = useState([]);
+  const [interviewInvitations, setInterviewInvitations] = useState([]);
+
+  const fetchInterviews = async (token) => {
+    try {
+      const response = await axios.get(`${serverUrl}/api/interviews/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUserInterviews(response.data);
+    } catch (error) {
+      console.log("Error fetching user interviews: ", error);
+    }
+  };
+
   return <interviewContext.Provider>{children}</interviewContext.Provider>;
 };
 
