a01e5458b5c2292ffb0ef32c17db03aa37274d7d
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-09 09:42:20 +0200

    Added video provider to app providers

--- src/contexts/AppProviders.js
+++ src/contexts/AppProviders.js
@@ -4,16 +4,19 @@ import DashboardProvider from "./DashboardContext/DashboardContext";
 import GeneralProvider from "./GeneralContext/GeneralContext";
 import InterviewProvider from "./InterviewContext/InterviewContext";
 import LearningProvider from "./LearningContext/LearningContext";
+import VideoProvider from "./VideoCallContext/VideoCallContext";
 
 const AppProviders = ({ children }) => (
   <AuthProvider>
     <GeneralProvider>
       <InterviewProvider>
-        <LearningProvider>
-          <CommunityProvider>
-            <DashboardProvider>{children}</DashboardProvider>
-          </CommunityProvider>
-        </LearningProvider>
+        <VideoProvider>
+          <LearningProvider>
+            <CommunityProvider>
+              <DashboardProvider>{children}</DashboardProvider>
+            </CommunityProvider>
+          </LearningProvider>
+        </VideoProvider>
       </InterviewProvider>
     </GeneralProvider>
   </AuthProvider>
