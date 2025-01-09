f11ec608b1530ffc067bafa9a36ce0e3c6f94c02
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-09 10:28:15 +0200

    Added start interview function to the video interview

--- src/pages/Interviews/VideoInterview/VideoInterview.jsx
+++ src/pages/Interviews/VideoInterview/VideoInterview.jsx
@@ -1,15 +1,46 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { videoContext } from "../../../contexts/VideoCallContext/VideoCallContext";
 
 const VideoInterview = () => {
   const { roomId, fetchRoom } = useContext(videoContext);
   const token = localStorage.getItem("token");
+  const [scriptsLoaded, setScriptsLoaded] = useState(false);
 
   useEffect(() => {
+    const loadDailyIframe = async () => {
+      if (!window.DailyIframe) {
+        const script = document.createElement("script");
+        script.src = "https://unpkg.com/@daily-co/daily-js";
+        script.async = true;
+        script.onload = () => console.log("DailyIframe SDK loaded");
+        document.body.appendChild(script);
+      }
+    };
+    loadDailyIframe();
+  }, []);
+
+  useEffect(() => {
+    const startInterview = async () => {
+      const roomUrl = await fetchRoom(token);
+      if (roomUrl && window.DailyIframe) {
+        const iframe = window.DailyIframe.createFrame({
+          iframeStyle: {
+            position: "relative",
+            width: "100%",
+            height: "100%",
+            border: "0",
+            zIndex: 9999,
+          },
+          showLeaveButton: true,
+          showFullscreenButton: true,
+        });
+        iframe.join({ url: roomUrl });
+      } else {
+        console.error("DailyIframe is not available or room URL is invalid.");
+      }
+    };
     if (roomId) {
-      const script = document.createElement("script");
-      script.innerHTML = fetchRoom(token);
-      document.body.appendChild(script);
+      startInterview();
     }
   }, [roomId]);
   return <div></div>;
