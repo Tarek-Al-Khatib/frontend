e7f95676e38496560a9c96b8bd0114085753e2bb
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-09 10:20:15 +0200

    Added rendering video call page on video interview

--- src/pages/Interviews/VideoInterview/VideoInterview.jsx
+++ src/pages/Interviews/VideoInterview/VideoInterview.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { videoContext } from "../../../contexts/VideoCallContext/VideoCallContext";
 
 const VideoInterview = () => {
+  const { roomId, fetchRoom } = useContext(videoContext);
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (roomId) {
+      const script = document.createElement("script");
+      script.innerHTML = fetchRoom(token);
+      document.body.appendChild(script);
+    }
+  }, [roomId]);
   return <div></div>;
 };
 
