84c0d640cc74aa7f2828c2e9c973389613046365
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-10 06:58:35 +0200

    Added setting messages and on message played func

--- src/contexts/ChatContext/ChatContext.js
+++ src/contexts/ChatContext/ChatContext.js
@@ -20,11 +20,24 @@ export const ChatProvider = ({ children }) => {
   const [message, setMessage] = useState();
   const [loading, setLoading] = useState(false);
 
+  const onMessagePlayed = () => {
+    setMessages((messages) => messages.slice(1));
+  };
+
+  useEffect(() => {
+    if (messages.length > 0) {
+      setMessage(messages[0]);
+    } else {
+      setMessage(null);
+    }
+  }, [messages]);
+
   return (
     <ChatContext.Provider
       value={{
         chat,
         message,
+        onMessagePlayed,
         loading,
       }}
     >
