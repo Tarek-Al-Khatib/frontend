e30a7548971dea19ab193da38ac185faf7fbe76a
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-10 06:58:10 +0200

    Added basic function to connect with chat on chat context

---
+++ src/contexts/ChatContext/ChatContext.js
@@ -0,0 +1,36 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { serverUrl } from "../../config/url";
+const ChatContext = createContext();
+
+export const ChatProvider = ({ children }) => {
+  const chat = async (message) => {
+    setLoading(true);
+    const data = await fetch(`${serverUrl}/chat`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    });
+    const resp = (await data.json()).messages;
+    setMessages((messages) => [...messages, ...resp]);
+    setLoading(false);
+  };
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState();
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <ChatContext.Provider
+      value={{
+        chat,
+        message,
+        loading,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export default ChatProvider;
