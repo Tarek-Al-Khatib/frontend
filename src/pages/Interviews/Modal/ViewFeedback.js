951ed995b908ca4900674a381c98639dfea9322e
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-12 08:31:43 +0200

    Adjusted styling of the view feedback modal

--- src/pages/Interviews/Modal/ViewFeedback.js
+++ src/pages/Interviews/Modal/ViewFeedback.js
@@ -16,7 +16,7 @@ const ViewFeedback = ({ text, open, handleClose }) => {
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
-          width: 300,
+          width: 600,
           bgcolor: "background.paper",
           boxShadow: 24,
           p: 4,
