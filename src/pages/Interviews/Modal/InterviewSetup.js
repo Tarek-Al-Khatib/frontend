d399327f6134ba93b43d2c78c659e47923072664
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-17 20:27:57 +0200

    Added voice ids for the interviewers

--- src/pages/Interviews/Modal/InterviewSetup.js
+++ src/pages/Interviews/Modal/InterviewSetup.js
@@ -24,40 +24,40 @@ const InterviewSetup = ({ isOpen, onClose, onInterviewSelect }) => {
       description: "Technical Interviewer with strict dialogue",
       characteristics:
         "You sometimes make fun of the candidate, and you are strict with your answers.",
-      voiceId: "",
+      voiceId: "29vD33N1CtxCmqQRPOHJ",
     },
     {
       label: "The Dev Lord",
       description: "Be careful, don't mess with him in coding!",
       characteristics:
         "You do not accept false answers to coding question. Other then that act like a normal HR interviewer. You are so calm and strict with answers ",
-      voiceId: "",
+      voiceId: "iP95p4xoKVk53GoZ742B",
     },
     {
       label: "The Recruitment General",
       description: "A sharp HR interviewer who values precision.",
       characteristics:
         "You pay full attention to details, and you are strict. You are not fun and you are picky",
-      voiceId: "",
+      voiceId: "ThT5KcBeYPX3keUQqHPh",
     },
     {
       label: "The Color Queen",
       description: "You can feel her flowery vibe.",
       characteristics: "Very fun, a cool and lovely HR",
-      voiceId: "",
+      voiceId: "AZnzlk1XvdvUeBnXmlld",
     },
     {
       label: "The Careerster",
       description: "Straight to the point questions? This one is for you.",
       characteristics:
         "You are energetic in a calm way, but you are also straight to the point",
-      voiceId: "",
+      voiceId: "oWAxZDx7w5VEj9dCyTzz",
     },
     {
       label: "WorkWise Interviewer",
       description: "The default interviewer avatar and tonality.",
       characteristics: "Default HR interviewer personality",
-      voiceId: "",
+      voiceId: "jsCqWAovK2LkecY7zXl4",
     },
   ];
 
